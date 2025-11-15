ed38adeda02bf58eb1eeedcad33799d640a0bb7e
OgborFaith <faithogbor23@gmail.com>
2025-11-15 02:56:17 +0100

    Added navigation setup, updated Header, Home, Posts, and Users pages

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import Link from "next/link";
+import Link from 'next/link'; 
 
 const Header: React.FC = () => {
-    return (
-<header className="w-full p-4 bg-gray-900 text-white flex justify-between">
-      <h1 className="text-xl">My App</h1>
-      <nav className="flex gap-4">
-        <Link href="/">Home</Link>
-        <Link href="/posts">Posts</Link>
-        <Link href="/users">Users</Link>
-      </nav>
+  return (
+    <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h3 className="font-bold text-2xl">
+     <Link href="/">Daily Contents</Link>
+  </h3>
+        <nav>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
-    )
+  );
 }
 
-export default Header
+export default Header;
